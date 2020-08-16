8125e41345f6b1833b76bfcebd85873913e5ad4f
Arseni Leskinen <arsenileskinen@gmail.com>
2020-08-16 04:50:25 +0300

    fine tune ui element sizes

--- frontend/pages/server-side-rendered-page.js
+++ frontend/pages/server-side-rendered-page.js
@@ -12,11 +12,8 @@ const Home = ({packages}) => {
   }
 
   const handleDependencyClick = (e) => {
-      let a = e.target.textContent
-      console.log("##############", a)
-      let b = packages.find(pkg => pkg.Package === e.target.textContent)
-      console.log("!!!!!!!!!!!!!!", b)
-      setSelectedPkg(b)
+      let targetpkg = packages.find(pkg => pkg.Package === e.target.textContent)
+      setSelectedPkg(targetpkg)
   }
 
   return (
@@ -27,10 +24,10 @@ const Home = ({packages}) => {
       </Head>
       <NavigationBar/>
 
-      <div class="flex flex-row py-6 px-6 pt-20 text-lg bg-gray-800">
+      <div class="flex flex-row py-6 px-6 pt-20 text-md bg-gray-800">
         <div class="p-6 w-4/12 min-w-sm max-w-md">
           {packages.map((pkg, index) => (
-            <div key={index} class="mb-4 bg-gray-100 rounded shadow-md text-center">
+            <div key={index} class="mb-4 bg-gray-100 rounded shadow-md text-center text-lg">
                 <button onClick={() => handleSelectPkg(pkg)}
                     class="rounded font-bold truncate w-full whitespace-no-wrap hover:bg-gray-300 focus:bg-gray-400 focus:outline-none border border-gray-400">
                     {pkg.Package}
@@ -41,15 +38,15 @@ const Home = ({packages}) => {
 
         {selectedPkg ? (
             <div class="p-6 w-full">
-                <div class="w-full p-8 sticky top-6 bg-gray-100 rounded shadow-md border border-gray-400 z-0">
-                    <p class="text-center text-3xl m-2 font-bold ">
+                <div class="w-full top-6 p-8 sticky bg-gray-100 rounded shadow-md border border-gray-400 z-0">
+                    <p class="text-center text-3xl font-bold ">
                         {selectedPkg.Package}
                     </p>
                     <p class="pt-8 font-bold">Description:</p>
-                    <p class="py-3 whitespace-pre-line">
+                    <p class="py-2 whitespace-pre-line">
                     {selectedPkg.Description}
                     </p>
-                    <p class="pt-8 font-bold">Dependencies:</p>
+                    <p class="pt-2 font-bold">Dependencies:</p>
                     {selectedPkg.Depends.map((dep, index) => (
                         <li key={index}
                             onClick = {(e) => handleDependencyClick(e)} 
@@ -57,7 +54,7 @@ const Home = ({packages}) => {
                             {dep}
                         </li>
                     ))}  
-                    <p class="pt-8 font-bold">Used by:</p>
+                    <p class="pt-2 font-bold">Used by:</p>
                     {selectedPkg.DependencyFor.map((dep, index) => (
                         <li key={index} 
                             onClick = {(e) => handleDependencyClick(e)}
