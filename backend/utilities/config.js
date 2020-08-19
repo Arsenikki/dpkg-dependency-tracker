18656dcd6a53d7c1196142642c6bf42cfcc3fa74
Arseni Leskinen <arsenileskinen@gmail.com>
2020-08-20 00:35:19 +0300

    Remove unneeded configs + update packages etc

--- backend/utilities/config.js
+++ backend/utilities/config.js
@@ -1,25 +1,9 @@
-const fs = require('fs');
-
 require('dotenv').config();
 
-const userPath = './data/user-dpkg-data.txt';
-const examplePath = './data/example-dpkg-data.txt';
-
-const checkUserFileExistance = () => {
-  if (fs.existsSync(userPath)) {
-    console.log('Found the user provided dpkg file');
-    return true;
-  }
-  console.log('No user dpkg file found, using the example file');
-  return false;
-};
-
-// Use user file if it's provided, else use example file.
-const filePath = checkUserFileExistance() ? userPath : examplePath;
-
+const defaultPath = './storage/';
 const port = process.env.PORT || 5000;
 
 module.exports = {
-  filePath,
+  defaultPath,
   port,
 };
