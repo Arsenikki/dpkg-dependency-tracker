4cd4bc7ba4f5ca6d2761ff29b64603a9adca4427
Arseni Leskinen <arsenileskinen@gmail.com>
2020-08-24 01:45:35 +0300

    Implemented file upload in frontend

--- frontend/components/navigation-bar.js
+++ frontend/components/navigation-bar.js
@@ -1,24 +1,41 @@
-import Link from 'next/link';
 
-export default function NavigationBar() {
+import { useState } from 'react';
+
+export default function NavigationBar({uploadFile}) {
+  const [uploadButtonColor, setUploadButtonColor] = useState("bg-orange-400")
+
+  const hiddenFileInput = React.useRef(null);
+
+  const handleClick = (e) => {
+    hiddenFileInput.current.click();
+  };
+
+  const buttonResultColorizer = (status) => {
+    status ? setUploadButtonColor("bg-green-200") : setUploadButtonColor("bg-red-600")
+    setTimeout(() => {
+      setUploadButtonColor("bg-orange-400")
+    }, 5000) 
+  }
+
+  const handleChange = async e => {
+    const fileToUpload = e.target.files[0];
+    const uploadResult = await uploadFile(fileToUpload);
+    console.log("Upload finished:", uploadResult)
+    buttonResultColorizer(uploadResult)
+  };
+
   return (
-    <nav class='flex justify-center fixed w-full bg-gray-100 px-8 py-2 shadow-md z-10'>
-      <Link href='/static-generated-page'>
-        <a
-          class='no-underline uppercase rounded hover:bg-gray-300 focus:bg-gray-400 focus:outline-none tracking-wide font-bold text-xs py-3 mr-8'
-          href='#'
-        >
-          Load static generated dpkg set
-        </a>
-      </Link>
-      <Link href='server-side-rendered-page'>
-        <a
-          class='no-underline uppercase rounded hover:bg-gray-300 focus:bg-gray-400 focus:outline-none tracking-wide font-bold text-xs py-3 mr-8'
-          href='#'
-        >
-          Load server side rendered dpkg set
-        </a>
-      </Link>
+    <nav className='flex fixed justify-center w-full bg-gray-100 px-8 py-2 shadow-md z-10'>
+      <button onClick={handleClick} className={`rounded text-white p-4 font-bold inline-block hover:bg-orange-300 ${uploadButtonColor} py-3`}>
+        Upload a file
+      </button>
+      <input
+        type="file"
+        ref={hiddenFileInput}
+        accept=".txt"
+        onChange={handleChange}
+        style={{display: 'none'}}
+      />
     </nav>
   );
 }
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,4 +1,16 @@
 import NavigationBar from '../components/navigation-bar';
+    const uploadFile = async (file) => {
+      let formData = new FormData();
+      formData.append("dpkg-file", file);
+      const res = await fetch('http://localhost:5000/api/upload', {
+        method: "POST", 
+        body: formData
+        });
+      const userPackages = await res.json();
+      handleChangePackages(userPackages);
+      console.log("userPackages", userPackages)
+      return res.status === 200 ? true : false 
+    };
 
 export default function Index() {
   return <NavigationBar />;
