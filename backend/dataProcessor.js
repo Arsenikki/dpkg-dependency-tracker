3f98ffa98e3fa9414b6940ee6fd62559b74de6ce
Arseni Leskinen <arsenileskinen@gmail.com>
2020-08-26 01:01:19 +0300

    refactoring + lil bit more regex

--- backend/dataProcessor.js
+++ backend/dataProcessor.js
@@ -1,27 +1,16 @@
 const { fileLoader } = require('./utilities/fileLoader');
 
-const alternativeDependencyList = [];
-
 const parseOnlyValueFromString = (fullString, stringToRemove) => fullString.replace(`${stringToRemove}: `, '');
 
-const splitAndRegexDependencies = (input, index) => {
-  const dependencies = input.split(', ');
+const splitAndRegexDependencies = (input) => {
+  // Split dependency string by comma and pipe character
+  const dependencies = input.split(/[|,]+/);
   const packageDependencies = dependencies.map((dependency) => {
     // Remove white spaces and version tags
     const depNoWhitespace = dependency.replace(/\s/g, '');
-    const depNoVersiontag = depNoWhitespace.replace(/ *\([^)]*\) */g, '');
-
-    // Save separately dependency alternatives marked by pipe character.
-    if (depNoVersiontag.includes('|')) {
-      const depForComparison = depNoVersiontag.split('|');
-      alternativeDependencyList.push({
-        index,
-        Depends: depForComparison,
-      });
-    }
-    return depNoVersiontag;
+    return depNoWhitespace.replace(/ *\([^)]*\) */g, '');
   });
-  return packageDependencies;
+  return packageDependencies || [];
 };
 
 const joinMultilineDescriptions = (separateLines) => {
@@ -29,7 +18,7 @@ const joinMultilineDescriptions = (separateLines) => {
   let descriptionStartedIndex;
 
   // Find all description lines
-  const multilineDescription = separateLines.map((line, index) => {
+  const multilineDescription = separateLines.filter((line, index) => {
     if (line.includes('Description')) {
       descriptionStartedIndex = index;
       return `${line}.`;
@@ -37,10 +26,12 @@ const joinMultilineDescriptions = (separateLines) => {
     if (descriptionStartedIndex && line.startsWith(' ')) {
       return line;
     }
+    return null;
   }).join(' ');
 
-  // Remove extra whitespaces & periods, and add newline to list items (* / -)
+  // Remove extra periods & whitespaces, and add newline to list items (* / -)
   const regexDescriptions = multilineDescription.replace(/ +\./g, '\n')
+    .replace(/\.\.+/g, '')
     .replace(/ +\./g, '')
     .replace(/\s\s-\s/g, '\n- ')
     .replace(/\s\*\s/g, '\n- ')
@@ -76,21 +67,7 @@ const dataParser = (rawdata) => {
   return packages;
 };
 
-const alternativeDependencyComparer = (parsedPackages) => {
-  const PkgsWithCorrectAlt = parsedPackages.map((pkg) => {
-    const PkgsWithAlts = pkg.Depends?.map((dep) => {
-      if (dep.includes('|')) {
-        const separatedAlts = dep.split('|');
-        return separatedAlts;
-      }
-      return dep;
-    });
-    return { ...pkg, Depends: PkgsWithAlts?.flat() || [] };
-  });
-  return PkgsWithCorrectAlt;
-};
-
-const findReverseDependency = (pkgName, pkgs) => pkgs.reduce((array, pkg) => ((pkg.Depends.some((dep) => dep === pkgName))
+const findReverseDependency = (pkgName, pkgs) => pkgs.reduce((array, pkg) => ((pkg.Depends?.some((dep) => dep === pkgName))
   ? [...array, pkg.Package]
   : array), []);
 
@@ -105,7 +82,7 @@ const addReverseDependencies = (allPkgs) => {
 const packageSorter = (pkgsToSort) => {
   const sortedPackages = [...new Set(pkgsToSort.sort((a, b) => a.Package.localeCompare(b.Package)))];
   return sortedPackages.map((pkg) => {
-    const sortedDeps = [...new Set(pkg.Depends.sort((a, b) => a.localeCompare(b)))];
+    const sortedDeps = [...new Set(pkg.Depends?.sort((a, b) => a.localeCompare(b)))];
     const sortedReverseDeps = [...new Set(pkg.DependencyFor.sort((a, b) => a.localeCompare(b)))];
     return { ...pkg, Depends: sortedDeps, DependencyFor: sortedReverseDeps };
   });
@@ -121,11 +98,8 @@ const dataProcessor = async (inputFile) => {
   // Parse and process the data.
   const parsedPackages = dataParser(rawdata);
 
-  // Compare and sellect correct alternative i.e. gpgv | gpgv2 | gpgv1
-  const dependencyCompared = alternativeDependencyComparer(parsedPackages);
-
   // Find reverse dependencies
-  const reverseDependenciesAdded = addReverseDependencies(dependencyCompared);
+  const reverseDependenciesAdded = addReverseDependencies(parsedPackages);
 
   // Sort dependencies alphabetically and remove duplicates
   const finalPackages = packageSorter(reverseDependenciesAdded);
